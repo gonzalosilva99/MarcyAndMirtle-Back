731a2b6ab40464d6b4081a66ff2886b05fec5259
Gonzalo <gonga.silva1999@gmail.com>
2022-03-26 20:55:22 -0300

    Orders added

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,11 @@ class Category < ApplicationRecord
     validates :name, presence: true
     validates :name, uniqueness: true
     has_many :products
+    
+    after_initialize :add_order
+
+    private 
+    def add_order
+        self.order = self.id if self.order.nil?
+    end 
 end
